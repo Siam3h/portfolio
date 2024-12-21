2f8956648d3d3ad35a58a0bdd20838599ad37cf8
siam3h <philosiama@gmail.com>
2024-12-21 21:07:16 +0300

    Add project files

---
+++ src/pages/projects/data.ts
@@ -0,0 +1,50 @@
+import type { ProjectData } from '@/types'
+
+export const projectData: ProjectData = [
+  {
+    title: 'Tech & Data',
+    projects: [
+      {
+        text: 'TidyDataCLI',
+        description: 'TidyDataCLI is an open-source command-line tool designed to streamline data preprocessing, analysis, and visualization for data professionals. The tool offers a comprehensive suite of functionalities, including automated data cleaning, text standardization, detailed summary reporting, and advanced data visualization.',
+        icon: 'i-carbon-campsite',
+        href: 'https://siam3h.github.io/TidyDataCLI/',
+      },
+      {
+        text: 'Live In Nairobi',
+        description: 'An events & ticketing platform where one can Discover, Book, and Share Events Across Kenya and is the Ultimate Guide to Nairobi\'s Hottest Events. Explore Events, Purchase Tickets, Post Your Own, and Read Exclusive Reviews!',
+        icon: 'i-carbon-campsite',
+        href: 'https://liveinnbo.co.ke',
+      },
+      {
+        text: 'AgriInsight',
+        description: 'A platform that empowers farming decisions with data-driven solutions. .',
+        icon: 'i-carbon-campsite',
+        href: 'https://agrinsight.vercel.app/',
+      },
+    ],
+  },
+  {
+    title: 'Planning & Research',
+    projects: [
+      {
+        text: 'Development Control',
+        description: 'Preparation of reports and submission of applications for changes in land use and use extensions, ensuring compliance with zoning laws and urban planning regulations to maintain sustainable development and urban harmony',
+        icon: 'i-carbon-campsite',
+        href: '/projects',
+      },
+      {
+        text: 'Research',
+        description: 'Lead a data collection and desktop study for a research project focusing on the publications on informal settlements through interrogating textual and contextual Polarization of scholarships and Knowledges of Slums in Kenya. ',
+        icon: 'i-carbon-campsite',
+        href: '/projects',
+      },
+      {
+        text: 'Landuse Masterplans & Site Plans',
+        description: 'Creating a comprehensive, mixed-use master plan for a University Community in Ewaso Kedong, Kajiado County. This project integrates residential, educational, and recreational spaces, aimed at fostering a sustainable and vibrant community while balancing environmental and social needs.',
+        icon: 'i-carbon-campsite',
+        href: '/',
+      },
+    ],
+  },
+]
---
+++ src/site-config.ts
@@ -0,0 +1,106 @@
+export const siteConfig = {
+  author: 'Philbert Otieno',
+  title: 'My personal website and portfolio',
+  subtitle: 'Showcasing My Work, Skills, and Projects.',
+  description: 'Welcome to my personal space on the web. This portfolio highlights my latest projects, skills, and achievements. Explore my work, read my blog posts, and get in touch to collaborate or learn more about my expertise',
+  image: {
+    src: '/hero.jpg',
+    alt: 'Website Main Image',
+  },
+  email: 'siamaphilbert@outlook.com',
+  socialLinks: [
+    {
+      text: 'GitHub',
+      href: 'https://github.com/siam3h',
+      icon: 'i-simple-icons-github',
+      header: 'i-ri-github-line',
+    },
+    {
+      text: 'Twitter(X)',
+      href: 'https://x.com/oti33n',
+      icon: 'i-simple-icons-x',
+      header: 'i-ri-twitter-x-line',
+    },
+    {
+      text: 'Linkedin',
+      href: 'https://linkedin.com/in/philosiama',
+      icon: 'i-simple-icons-linkedin',
+    },
+    {
+      text: 'Instagram',
+      href: 'https://instagram.com/oti3n',
+      icon: 'i-simple-icons-instagram',
+    },
+    {
+      text: 'Youtube',
+      href: 'https://youtube.com/siamaphilbert',
+      icon: 'i-simple-icons-youtube',
+    },
+  ],
+  header: {
+    logo: {
+      src: '/favicon.ico',
+      alt: 'Logo Image',
+    },
+    navLinks: [
+      {
+        text: 'Blog',
+        href: '/blog',
+      },
+      {
+        text: 'Notes',
+        href: '/blog/notes',
+      },
+      {
+        text: 'Talks',
+        href: '/blog/talks',
+      },
+      {
+        text: 'Projects',
+        href: '/projects',
+      },
+    ],
+  },
+  page: {
+    blogLinks: [
+      {
+        text: 'Blog',
+        href: '/blog',
+      },
+      {
+        text: 'Notes',
+        href: '/blog/notes',
+      },
+      {
+        text: 'Talks',
+        href: '/blog/talks',
+      },
+    ],
+  },
+  footer: {
+    navLinks: [
+      {
+        text: 'Home',
+        href: '/',
+      },
+      {
+        text: 'Blog',
+        href: '/blog',
+      },
+      {
+        text: 'Wanna talk?',
+        href: '/contact',
+      },
+      {
+        text: 'Urban Planning & Research',
+        href: '/projects',
+      },
+      {
+        text: 'Data & Tech',
+        href: '/projects',
+      },
+    ],
+  },
+}
+
+export default siteConfig
